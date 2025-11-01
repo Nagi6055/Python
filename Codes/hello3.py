a0f09e5016b1d5645a6be76483d4bff2d4b1a377
Nagi <133611247+Nagi6055@users.noreply.github.com>
2025-11-01 14:53:00 -0500

    Add files via upload

---
+++ Codes/hello3.py
@@ -0,0 +1,21 @@
+word1 = "Hello "
+word2 = "World!"
+
+print(word1 + word2)
+
+
+#Adding a Space to Print 
+
+word1 = "Hello"
+word2 = "World!"
+
+print(word1 + " " + word2)
+
+#Using a Third Variable
+
+word1 = "Hello"
+word2 = "World!"
+space = " "
+
+print(word1 + space + word2)
+
